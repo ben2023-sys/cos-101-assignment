674a2f26152ed9cbb6552d269b5b442ea061a931
iroben <irobenedict81@gmail.com>
2024-12-05 01:31:03 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-igbo_to_english = {
-    "akwa": "cloth",
-    "ụlọ": "house",
-    "ụzọ": "road",
-    "azụ": "fish",
-    "chi": "god",
-    "nnọọ": "hello",
-    "ezi": "good",
-    "anyị": "we",
-    "ọtụtụ": "many",
-    "nwanne": "sibling",
+for i in range(10, 0, -1):
+    print(i)
 
-}
-
-# Example of using the dictionary to translate Igbo words to English
-word = "akwa"
-if word in igbo_to_english:
-    print(f"The English translation of '{word}' is '{igbo_to_english[word]}'.")
-else:
-    print(f"'{word}' not found in dictionary.")
